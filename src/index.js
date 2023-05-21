667db30f9c5d5d7d9e90dc3aad4085b3f480f568
011bq-001 <011bq-001@011bq-001s-MacBook-Air.local>
2023-05-21 12:39:25 +0530

    setup the store.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import './index.css';
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
-import './index.css';
 
 const el = document.getElementById("root");
 const root = createRoot(el);
 
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
