714bf41c9d9ff2e6a0e4b2a148f63a6b2cdfa750
011bq-001 <011bq-001@011bq-001s-MacBook-Air.local>
2023-07-16 20:54:10 +0530

    Added loading in add user button and disabled it till user adds (api returns).

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
-import   Button  from "./Button"
+import Button  from "./Button"
 import Skeleton from "./Skeleton";
 import useThunk from "../hooks/useThunk";
 
@@ -42,11 +42,7 @@ function UsersList() {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {
-          isCreatingUser
-          ? 'Creating User...'
-          : <Button onClick={handleAddUser} >+ Add User</Button>
-        }
+        <Button loading={isCreatingUser} onClick={handleAddUser} >+ Add User</Button>
         {creatingUserError && 'Error creating user... '}
       </div>
       {renderedUsers}
